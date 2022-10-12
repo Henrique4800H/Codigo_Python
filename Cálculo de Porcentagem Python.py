e024192481c32bfccf8a05fb07b33514df87a12b
Henrique4800H <115599561+Henrique4800H@users.noreply.github.com>
2022-10-12 01:37:07 -0300

    Código de porcentagem
    Código de Porcentagem Simples em Python

---
+++ Cálculo de Porcentagem Python.py
@@ -0,0 +1,8 @@
+print("Vou ser um dev Python custe o que custar!")
+print("erro aqui erro ali, resolvendo e evoluindo")
+Porcentagem = (int(input("qual porcentagem? ")))
+valor = (int(input("de qual valor? ")))
+resultado =(int(valor / 100 * Porcentagem))
+print(resultado)
+input()
+#input no final para não fechar automaticamente ao ser aberto no cmd
