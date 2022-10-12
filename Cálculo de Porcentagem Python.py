d594936299b8799ceabb13f24a2cf2c18b7d5028
Henrique4800H <henriquej37_@hotmail.com>
2022-10-12 12:02:49 -0300

    arquivo alterado e arquivo removido

--- Cálculo de Porcentagem Python.py
+++
@@ -1,8 +0,0 @@
-print("Vou ser um dev Python custe o que custar!")
-print("erro aqui erro ali, resolvendo e evoluindo")
-Porcentagem = (int(input("qual porcentagem? ")))
-valor = (int(input("de qual valor? ")))
-resultado =(int(valor / 100 * Porcentagem))
-print(resultado)
-input()
-#input no final para não fechar automaticamente ao ser aberto no cmd
